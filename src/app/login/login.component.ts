935c24e796bf3a4340561da4a2708d671eb7a5f4
Swami <anilsanapathi9@gmail.com>
2024-03-09 14:30:57 +0530

    refactor: add autofocus on login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -26,6 +26,7 @@ import { LoginService } from './login.service';
                                     type="text"
                                     placeholder="Email"
                                     autocomplete="off"
+                                    autofocus
                                     formControlName="email"
                                 />
                             </fieldset>

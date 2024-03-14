0780f844222d358955948b3fd57ee78bdb657a25
Swami <anilsanapathi9@gmail.com>
2024-03-14 23:46:33 +0530

    feat: add openapi-gen

---
+++ src/app/shared-data-access-api/fn/tags/get-tags.ts
@@ -0,0 +1,35 @@
+/* tslint:disable */
+/* eslint-disable */
+import { HttpClient, HttpContext, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
+import { RequestBuilder } from '../../request-builder';
+import { StrictHttpResponse } from '../../strict-http-response';
+
+export interface GetTags$Params {}
+
+export function getTags(
+    http: HttpClient,
+    rootUrl: string,
+    params?: GetTags$Params,
+    context?: HttpContext
+): Observable<
+    StrictHttpResponse<{
+        tags: Array<string>;
+    }>
+> {
+    const rb = new RequestBuilder(rootUrl, getTags.PATH, 'get');
+    if (params) {
+    }
+
+    return http.request(rb.build({ responseType: 'json', accept: 'application/json', context })).pipe(
+        filter((r: any): r is HttpResponse<any> => r instanceof HttpResponse),
+        map((r: HttpResponse<any>) => {
+            return r as StrictHttpResponse<{
+                tags: Array<string>;
+            }>;
+        })
+    );
+}
+
+getTags.PATH = '/tags';
